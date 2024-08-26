3517e13b4e102e2d64edbbfb2c516b81c409b037
pablomartan <pablomartan@kolabnow.com>
2024-08-26 14:37:18 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Home Manager configuration of pablo";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:pablomartan/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nixvim,
+    ...
+  }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."pablo" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [
+        ./home.nix
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              neovim = nixvim.packages.${system}.default;
+            })
+          ];
+        }
+      ];
+    };
+  };
+}
