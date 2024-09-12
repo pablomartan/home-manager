a93e80560e14afed2914815fa5daec469072ad82
Pablo Martínez <pablomartan@kolabnow.com>
2024-09-12 10:22:57 +0200

    feat: add firefox config

--- config.nix
+++ config.nix
@@ -201,5 +201,42 @@
         bind 'c' new-window -c "#{pane_current_path}"
       '';
     };
+
+    firefox = {
+      enable = true;
+
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+          settings = {
+            "browser.search.defaultenginename" = "DuckDuckGo";
+            "browser.search.order.1" = "DuckDuckGo";
+
+            "signon.rememberSignons" = false;
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.compactmode.show" = true;
+            "browser.cache.disk.enable" = false; # Be kind to hard drive
+
+            "mousewheel.default.delta_multiplier_x" = 20;
+            "mousewheel.default.delta_multiplier_y" = 20;
+            "mousewheel.default.delta_multiplier_z" = 20;
+
+            # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+            # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+            # then have Firefox open on some other workspace.
+            "widget.disable-workspace-management" = true;
+          };
+
+          search = {
+            force = true;
+            default = "DuckDuckGo";
+            order = ["DuckDuckGo"];
+          };
+        };
+      };
+    };
   };
 }
