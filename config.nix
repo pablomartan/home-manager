7f72ea3e1d3371a1682aba1245c64a5cdd716b69
Pablo Martínez <pablomartan@kolabnow.com>
2025-08-07 22:52:47 +0200

    fix(gnome): delete all switch to application keybindings

--- config.nix
+++ config.nix
@@ -101,6 +101,10 @@ in {
       switch-to-application-2 = [];
       switch-to-application-3 = [];
       switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
     };
   };
 
